c35c7738fcc4095aff198b3a33db81611f8866b8
준서 <70479192+JunseoKR@users.noreply.github.com>
2021-05-14 19:48:01 +0900

    Use def Function
    def Function, User Information Get

--- Main.py
+++ Main.py
@@ -21,18 +21,44 @@ import urllib.request
 
 """ [ 메인 ] --------------------------------------------------------------------------------------------------- """
 
+
+# USER Information Check
+
+def User():
+    ID = input("학번을 입력하세요 : ")
+    Name = input("\n이름을 입력하세요 : ")
+    print("학번 : ",ID)
+    print("이름: ",Name)
+    print("\n[ 확인되었습니다 ]")
+
+
+
 # FTP Server State Check [ URL Download ]
 
-url = 'http://datajunseo.ipdisk.co.kr:8000/list/HDD1/Server/ZOSC/INF/CHECK_INF.txt'
-res = urllib.request.urlopen(url)
-data = res.read()
+def State():
 
-State = data.decode("utf-8")
+    url = 'http://datajunseo.ipdisk.co.kr:8000/list/HDD1/Server/ZOSC/INF/CHECK_INF.txt'
 
-if State == "NORMAL":
-    {
-        print("FTP ON")}
+    res = urllib.request.urlopen(url)
 
-else:
-    {
-        print(exit)}
+    data = res.read()
+
+    State = data.decode("utf-8")
+
+    if State == "NORMAL":
+        print("\nFTP ONLINE")
+
+    else:
+        print("\nFTP OFFLINE")
+        print(exit)
+
+
+
+
+
+
+
+
+User()
+
+State()
